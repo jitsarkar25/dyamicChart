c71110a54be5d1ac57146561a615eb12aba89227
Jit Sarkar <jit.sarkar25@gmail.com>
2017-03-19 18:18:24 +0530

    added support for various chart types

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 function addclick(){
-    var dchart=new dynamicchart();
+   
     dchart.createdata();
+    //console.log(dchart.labels)
 }
 
 var count = 0;
-var dynamicchart =function ()
+var Dynamicchart =function ()
 {
     this.chartElm="";
     this.data="";
     this.container = document.getElementById("datainputcontainer");
+    this.typeselect = document.getElementById("typeselect");
     this.labels=[];
     this.values=[];
+    this.typeselect.addEventListener("change",this.createChart.bind(this));
     
 }
-dynamicchart.prototype.createdata = function(){ 
+Dynamicchart.prototype.createdata = function(){ 
+      var that=this;
+   // console.log(that);
   count++;    
   console.log("click");  
   
@@ -30,13 +35,13 @@ dynamicchart.prototype.createdata = function(){
   var updatebtn = document.createElement("BUTTON");
   updatebtn.innerHTML="Update";
   updatebtn.id="updatebtn"+count;  
-    var that=this;
+  
   updatebtn.addEventListener("click",function()
                             {
-      console.log(inputBoxValue.value);
-  });
-    
-    
+     
+      that.updateLabel(inputBoxLabel.value,inputBoxValue.value);
+      //that.values.push(inputBoxValue.value);
+  });   
   var text = document.createElement("TEXT");
   text.innerHTML="<br/><br/>";
   this.container.appendChild(inputBoxLabel);
@@ -44,3 +49,111 @@ dynamicchart.prototype.createdata = function(){
   this.container.appendChild(updatebtn);
   this.container.appendChild(text);
 };
+
+Dynamicchart.prototype.createJSON=function()
+{
+    var that=this;
+       console.log(that.labels);
+       console.log(that.values);
+    that.data=[];
+    for( var i = 0 ; i < that.labels.length ; i++ )
+        {
+            var dataObj ={
+              "label" : that.labels[i], 
+              "value" : that.values[i]  
+            };
+            that.data.push(dataObj);
+        }
+    
+    
+  
+    
+    that.moredata={
+        "chart": {
+        "caption": "Monthly revenue for last year",
+        "subCaption": "Harry's SuperMart",
+        "xAxisName": "Month",
+        "yAxisName": "Revenues (In USD)",
+        "numberPrefix": "$",
+        "theme": "fint"
+    },
+        "data" : that.data
+    };
+      console.log(that.chartType);
+    
+    /**/
+    that.createChart();
+   
+}
+
+Dynamicchart.prototype.createChart=function(){
+    
+    
+    var that=this;
+    
+    if(!that.moredata)
+        {
+            return;
+        }
+    var type = "pie2d"
+    
+
+        var selected=that.typeselect.value;
+        console.log("Selected  " + selected);
+        
+        if(selected === "Column")
+            {
+            type="column2d";
+                 console.log("column");
+            }
+        else if(selected === "Bar")
+            {
+            type="bar2d";
+                console.log("bar");
+            }
+        else if(selected === "Pie")
+            {
+            type = "pie2d";
+                console.log("pie");
+            }
+        
+       
+        console.log(selected === "bar");
+
+    
+     FusionCharts.ready(function(){
+      var revenueChart = new FusionCharts({
+        "type": type,
+        "renderAt": "chartContainer",
+        "width": "500",
+        "height": "300",
+        "dataFormat": "json",
+        "dataSource": JSON.stringify(that.moredata)
+    });
+
+    revenueChart.render();
+});
+}
+  
+Dynamicchart.prototype.updateLabel=function(inputlabel,inputvalue)
+{
+    var that=this;
+    var index= that.labels.indexOf(inputlabel);
+    if(index === -1)
+        {
+            that.labels.push(inputlabel)
+            that.values.push(inputvalue);
+        }
+    else
+        {
+            that.labels[index] = inputlabel;
+            that.values[index] = inputvalue;
+        }
+    
+    that.createJSON(that);
+    
+}
+
+window.addEventListener('load', function () {
+    window.dchart = new Dynamicchart();
+});
